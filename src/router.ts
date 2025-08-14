bb3542bb2d34fc89aea5a4d88708b7ab92c62d15
LucasRohr1 <91035404+LucasRohr1@users.noreply.github.com>
2025-08-14 13:50:34 -0300

    Primer validation en PUT de product
    Se agrega validacion generica => body.isString()

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
@@ -12,7 +13,18 @@ router.get('/product', (req, res) => {
 
 });
 router.get('/product/:id', () => {});
-router.put('/product/:id', () => {});
+
+router.put('/product/:id', body('name').isString(),(req, res) => {
+
+    const errors = validationResult(req);
+    console.log(errors);
+    
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.json({ message: "ok" });            
+});
+
 router.post('/product', () => {});
 router.delete('/product/:id', () => {});
 
