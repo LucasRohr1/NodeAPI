993de8787d1b45af31d84b834ccf3ace6c91940a
LucasRohr1 <91035404+LucasRohr1@users.noreply.github.com>
2025-08-14 14:32:59 -0300

    Middleware agregado en rutas de Router.ts
    Se valida que la request pase los datos requeridos requeridos por el schema

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { body, validationResult } from "express-validator";
+import { body, oneOf, validationResult } from "express-validator";
 import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
@@ -13,14 +13,20 @@ router.get('/product', (req, res) => {
         message: req.shhh_secret})
 
 });
+
 router.get('/product/:id', () => {});
 
 router.put('/product/:id', body('name').isString(), handleInputErrors,(req, res) => {
 
-    res.json({ message: "ok" });            
+    res.status(200);
+    res.json({ message: "ok" });          
 });
 
-router.post('/product', () => {});
+router.post('/product', body('name').isString(), handleInputErrors,(req, res) => {
+
+    res.status(200);
+    res.json({ message: "ok" }); 
+});
 router.delete('/product/:id', () => {});
 
 
@@ -30,18 +36,40 @@ router.delete('/product/:id', () => {});
 
 router.get('/update', () => {});
 router.get('/update/:id', () => {});
-router.put('/update/:id', () => {});
-router.post('/update', () => {});
+router.put('/update/:id',   
+        body('title').optional(), 
+        body('body').optional(), 
+        body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
+        body('version').optional(),
+        () => {}
+);
+
+router.post('/update', 
+        body('title').exists().isString(), 
+        body('body').exists().isString(), 
+        () => {}
+)
+
 router.delete('/update/:id', () => {});
 
 /**
- * Update Item
+ * UpdateItem
  */
 
 router.get('/updateitem', () => {});
 router.get('/updateitem/:id', () => {});
-router.put('/updateitem/:id', () => {});
-router.post('/updateitem', () => {});
+
+router.put('/updateitem/:id', 
+    body('name').optional().isString(), 
+    body('description').optional().isString(),
+    () => {});
+
+router.post('/updateitem', 
+    body('name').isString(), 
+    body('description').isString(),
+    body('updateId').exists().isNumeric(),
+    () => {});
+
 router.delete('/updateitem/:id', () => {});
 
 
